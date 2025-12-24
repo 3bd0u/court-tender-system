d4012d075d60262b4377b69bdcbd07eb0954f002
3bd0u <a.bellatreche@esi-sba.dz>
2025-12-24 10:46:33 +0100

    Fix: Add PYTHONPATH to conftest.py for CI/CD

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Ajouter le dossier parent au PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 from app import create_app, db
 from app.models import User, Candidate, Project
